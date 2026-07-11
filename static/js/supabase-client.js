d5d1459d81949c89727842deba5a34d8830080f8
polina-kolinko <151038895+polina-kolinko@users.noreply.github.com>
2026-07-11 16:01:07 +0300

    Create supabase-client.js

---
+++ static/js/supabase-client.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = 'https://znnrxmmbfgsabaacxggb.supabase.co';
+const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_3GJlFjIDSmF3QzZwrWeYrw_DB4rQn6y';
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
