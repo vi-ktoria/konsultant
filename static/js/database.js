f3be181dfc8f85af4b1b5907a71b0a1ac8a5b5cc
polina-kolinko <151038895+polina-kolinko@users.noreply.github.com>
2026-07-11 12:47:31 +0300

    Update database.js

--- static/js/database.js
+++ static/js/database.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = 'https://znnrxmmbfgsabaacxggb.supabase.co/rest/v1/';
+const SUPABASE_URL = 'https://znnrxmmbfgsabaacxggb.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_3GJlFjIDSmF3QzZwrWeYrw_DB4rQn6y';
 
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
